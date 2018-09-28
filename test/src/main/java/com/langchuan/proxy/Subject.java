04c6b30683fcf33073282af55aecaee8187ee0ab
kevin <18855970996@163.com>
2018-09-28 15:07:48 +0800

    1

---
+++ test/src/main/java/com/langchuan/proxy/Subject.java
@@ -0,0 +1,10 @@
+package com.langchuan.proxy;
+
+/**
+ * @author: kevin.xiong
+ * @description:
+ * @date:2018/9/28 15:03
+ */
+public interface Subject {
+
+}
---
+++ test/src/main/java/com/langchuan/test/Test01.java
@@ -0,0 +1,10 @@
+package com.langchuan.test;
+
+/**
+ * @author: kevin.xiong
+ * @description:
+ * @date:2018/9/28 15:04
+ */
+public class Test01 {
+
+}
